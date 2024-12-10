6fc9c579c5803fac803e636b3c7fa0992209f28e
arupkrm <100683950+arupkrm@users.noreply.github.com>
2024-12-10 11:29:12 +0530

    upload

---
+++ header.php
@@ -0,0 +1,50 @@
+
+
+
+
+
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <img src="frontend/images/logo.png" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="about.php">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="contact.php">Contact Us</a>
+                        </li>
+
+                        <!-- <li class="nav-item"> -->
+                            <!-- <a class="nav-link" href="admin/login.html" target="_blank">Admin</a> -->
+                        <!-- </li> -->
+                        <!-- <li class="nav-item"> -->
+                            <!-- <a class="nav-link" href="coaching/login.html" target="_blank">Coaching Center</a> -->
+                        <!-- </li> -->
+
+                        <li class="nav-item">
+                            <div class="bg-btn">
+                                
+                                <!--<a class="primary-btn btn-shadow red-btn" data-bs-toggle="offcanvas"
+                                    data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" role="button">-->
+
+                                    <a class="primary-btn btn-shadow red-btn">Let's Connect</a>
+
+                                <span></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
